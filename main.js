0f9887ffbdfbc1ffcdbb7108056ea607a182a9de
evsasse <evsasse@gmail.com>
2016-11-03 23:39:38 -0200

    buscar ordernacoes das configuracoes eh responsabilidade do servico, nao do cliente

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let config = require("./config.js");
 function clienteDoServico(){
   let unorderedData = config.defaultData;
 
-  let orderedData = servicoDeOrdenacao(unorderedData.slice(),config.orders);
+  let orderedData = servicoDeOrdenacao(unorderedData.slice());
 
   console.log(orderedData);
 }
 
-function servicoDeOrdenacao(data,orders){
+function servicoDeOrdenacao(data,orders = config.orders){
   if(!orders) throw "OrderingException";
   if(orders.length === 0) return [];
 
